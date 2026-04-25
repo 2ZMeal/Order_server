66bd700048401989d96e7cc8ca292a559f9a8432
yeonheeMoon <xsss7@naver.com>
2026-04-25 14:21:23 +0900

    init module

---
+++ order/src/main/java/com/ezmeal/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.ezmeal.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
