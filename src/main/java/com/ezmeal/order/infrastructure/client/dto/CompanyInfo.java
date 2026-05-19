d438c29106bd508f9df3bcb405c3b16c55a55a36
hjk2132 <xiest@naver.com>
2026-05-20 01:37:41 +0900

    hotfix: dto에 getter/setter 추가

--- src/main/java/com/ezmeal/order/infrastructure/client/dto/CompanyInfo.java
+++ src/main/java/com/ezmeal/order/infrastructure/client/dto/CompanyInfo.java
@@ -2,8 +2,12 @@ package com.ezmeal.order.infrastructure.client.dto;
 
 import lombok.Getter;
 import java.util.UUID;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class CompanyInfo {
     private UUID companyId;
     private String name;
--- src/main/java/com/ezmeal/order/infrastructure/client/dto/ProductInfo.java
+++ src/main/java/com/ezmeal/order/infrastructure/client/dto/ProductInfo.java
@@ -2,8 +2,12 @@ package com.ezmeal.order.infrastructure.client.dto;
 
 import lombok.Getter;
 import java.util.UUID;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class ProductInfo {
     private UUID productId;
     private UUID companyId;
